bc1c7e65fb7bb965be27bf90c0bd0412e81b488d
Dave Allen <tech@gyu.nu>
2016-10-13 23:02:24 +0900

    fix: emails now working on contact page, removed resume template

--- dist/site/templates/contact.php
+++ dist/site/templates/contact.php
@@ -1,3 +1,28 @@
+<?php
+
+if(get('contact', false)) {
+	$contact = get('contact');
+	if(isset($contact['page'])) {
+		$name = $contact['page']['name'];
+		$email_address = $contact['page']['email'];
+		$telephone_number = $contact['page']['phone'];
+		$company = $contact['page']['company'];
+		$message = $contact['page']['message'];
+		//TODO fix zoho.
+		$mail = new Email([
+			'to' => c::get('email.recipient'),
+			'from' => 'hello@gyu.nu',
+			'subject' => $name . ' has contacted you',
+			'body'    => $message . ' email: ' . $email_address . ' phone: ' . $telephone_number . ' company: ' . $company . ' message: ' . $message,
+			'service' => 'mail'
+		]);
+
+		if($mail->send()) {}
+	}
+}
+
+?>
+
 <?php snippet('header') ?>
 
   <main class="main" role="main">
@@ -9,25 +34,25 @@
 					<h4><em>Contact me via the form below or email me using the button below</em></h4>
 					<a class="leader" href="mailto:<?php echo $site->email() ?>"><?php echo $site->email() ?></a>
 				</div>
-				<form class="xs-12">
+				<form class="xs-12" action="" method="POST">
 					<fieldset>
 						<legend>Your Details</legend>
 						<label>Name
-							<input type="text" name="name" placeholder="John Smith"/>
+							<input type="text" name="contact[page][name]" placeholder="John Smith"/>
 						</label>
 						<label>Email address
-							<input type="email" name="email" placeholder="john.smith@johnsmith.com"/>
+							<input type="email" name="contact[page][email]" placeholder="john.smith@johnsmith.com"/>
 						</label>
 						<label>Telephone Number
-							<input type="tel" name="phone" placeholder="+81-123-456-789"/>
+							<input type="tel" name="contact[page][phone]" placeholder="+81-123-456-789"/>
 						<label>Company
-							<input type="text" name="company" placeholder="John Smith Incorporated."/>
+							<input type="text" name="contact[page][company]" placeholder="John Smith Incorporated."/>
 						</label>
 					</fieldset>
 					<fieldset>
 						<legend>Your Message</legend>
 						<label>Message
-							<textarea name="message"></textarea>
+							<textarea name="contact[page][message]"></textarea>
 						</label>
 					</fieldset>
 					<button type="submit" class="btn"><span class="ion ion-paper-airplane"></span> Submit</button>
