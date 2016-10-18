aad24674612bb53842d4e73db0661bb83869ddea
Dave Allen <da@dadi.co>
2016-10-18 12:11:48 +0900

    feature: some meta tags filled out, published check added to page

--- dist/site/snippets/header.php
+++ dist/site/snippets/header.php
@@ -5,7 +5,9 @@
 		<meta charset="utf-8">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title><?php echo $site->title()->html() ?></title>
-		<meta name="description" content="">
+		<meta name="description" content="<?php echo $site->description() ?>">
+		<meta name="title" content="<?php echo $site->title() ?>">
+		<meta name="keywords" content="<?php echo $site->keywords() ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
 		<link rel="icon" href="/favicon.png" type="image/x-icon">
