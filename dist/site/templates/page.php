4c3bef0f9a427d545a4df648854c730a785e0bc1
Dave Allen <da@dadi.co>
2016-10-18 12:12:29 +0900

    chore: added published check to blueprints

--- dist/site/templates/page.php
+++ dist/site/templates/page.php
@@ -3,7 +3,10 @@
   <main class="main" role="main">
 
 		<?php foreach($elements as $element): ?>
-			<?php snippet($element->intendedTemplate(), ['data' => $element]) ?>
+			<?php if($element->published()->bool()): ?>
+				<?php snippet($element->intendedTemplate(), ['data' => $element]) ?>
+			<?php endif ?>
+
 		<?php endforeach ?>
 
   </main>
